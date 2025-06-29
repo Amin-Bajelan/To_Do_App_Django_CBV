0a338209abe544938d0b3d354c258803d82f468d
Amin-Bajelan <m.amin.bajelan@gmail.com>
2025-06-29 11:38:31 +0330

    create authentication for api

--- task/api/v1/urls.py
+++ task/api/v1/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('list_task/', views.TaskListApiView.as_view(), name='task-list'),
-
+    path('api-auth/', include('rest_framework.urls')),
+    path('list_task/', views.TaskListApiView.as_view(), name='task-list_api'),
+    path("list_task/<int:pk>/", views.TaskDetailApiView.as_view(), name='task-detail_api'),
 ]
--- task/api/v1/views.py
+++ task/api/v1/views.py
@@ -1,11 +1,17 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import mixins
 from rest_framework.permissions import IsAdminUser
 from .serializers import TaskSerializer
 from ...models import Task
 
 
-class TaskListApiView(ListCreateAPIView,IsAdminUser):
+class TaskListApiView(ListCreateAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TaskDetailApiView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAdminUser]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
